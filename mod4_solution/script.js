278167a49d5dedbcda4393af233357c985e6e8d4
jyotikarthik <93524563+jyotikarthik@users.noreply.github.com>
2022-02-11 23:20:31 +0530

    Add files via upload

---
+++ mod4_solution/script.js
@@ -0,0 +1,29 @@
+var names = "Jyoti";
+function sayhello(){
+    console.log("Hello "+ names);
+}
+sayhello();
+
+var names ="Isha";
+function sayhi(){
+    console.log("Hi "+ names);
+}
+sayhi();
+
+var names = "Kunal";
+function sayhello(){
+    console.log("Hello "+ names);
+}
+sayhello();
+
+var names = "Shilpa";
+function sayhello(){
+    console.log("Hello "+ names);
+}
+sayhello();
+
+var names = "Batman";
+function sayhello(){
+    console.log("Hello "+ names);
+}
+sayhello();
---
+++ mod4_solution/scriptbye.js
@@ -0,0 +1,29 @@
+var names = "Jyoti";
+function saybye(){
+    console.log("Goodbye "+ names);
+}
+saybye();
+
+var names = "Isha";
+function saybye(){
+    console.log("Goodbye "+ names);
+}
+saybye();
+
+var names = "Kunal";
+function saybye(){
+    console.log("Goodbye "+ names);
+}
+saybye();
+
+var names = "Shilpa";
+function saybye(){
+    console.log("Goodbye "+ names);
+}
+saybye();
+
+var names = "Batman";
+function saybye(){
+    console.log("Goodbye "+ names);
+}
+saybye();
